b54a92a7d93815d5dad64bb9b04088ba985a364a
BlueGangster <garciachrisg1506@gmail.com>
2026-05-19 00:28:10 +0200

    Finished template home/design. WIP projects/design

--- app.py
+++ app.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():  # put application's code here
-    return render_template("index.html")
+    return render_template("pages/home.html")
+
+@app.route('/projects')
+def projects():  # put application's code here
+    return render_template("pages/projects/index.html", projects=
+                           [
+                               ["111111111", "Project 1", "Description of project 1"],
+                               ["222222222", "Project 2", "Description of project 2"],
+                               ["333333333", "Project 3", "Description of project 3"],
+                               ["444444444", "Project 4", "Description of project 4"]
+                           ])
 
 
 if __name__ == '__main__':
